5795be540042ba383866a0bdae2ff494685c9f46
Antonioma03 <97135229+Antonioma03@users.noreply.github.com>
2022-06-01 16:51:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function animales(){
+    window.location="animales.html";
+}
+function carro(){
+    window.location="carro.html";
+}
+function familia(){
+    window.location="familia.html";
+}
+function ofisina(){
+    window.location="ofisina.html";
+}
+function plantas(){
+    window.location="plantas.html";
+}
+
---
+++ plantas.js
@@ -0,0 +1,46 @@
+function atras(){
+    window.location="index.html";
+}
+function setup(){
+    canvas=createCanvas(700,400);
+    canvas.center();
+    detector=ml5.objectDetector('cocossd',modelocargado);
+document.getElementById("estatus").innerHTML="detectandoobjetos";
+}
+imagen="";
+function preload(){
+    imagen=loadImage('plantas.jpg');
+}
+function draw(){
+    image(imagen,0,0,700,400);
+if(estatus!=""){
+    for(i=0; i<objetos.length; i++){
+      document.getElementById("estatus").innerHTML="objetos detectados";
+      fill('#ff0000');
+      noFill();
+      stroke('#ff0000');
+      presision=floor(objetos[i].confidence*100);
+      text(objetos[i].label+ presision+"%",objetos[i].x,objetos[i].y);
+      rect(objetos[i].x,objetos[i].y,objetos[i].width,objetos[i].height);
+    } 
+}
+}
+estatus=""
+objetos=[];
+function modelocargado(){
+    console.log('modelocargado');
+    estatus=true;
+    detector.detect(imagen,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objetos=results;
+    }
+}
+
+
+
